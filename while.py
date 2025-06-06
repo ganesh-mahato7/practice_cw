2a6118ea09916bf0fe29633b349f72d734c8c67f
ganesh-mahato7 <ganeshmahato1412@gamil.com>
2025-06-06 10:08:09 +0545

    banking pass

--- while.py
+++ while.py
@@ -31,3 +31,29 @@
 # while i in range(4):
 #     print(a[i])
 #     i=i+1
+
+# a=[1,2,3,4]
+# i=0
+# while i<len(a):
+#     if a[i]==1 or a[i]==3:
+#         i=i+1
+#         continue
+#     else:
+#      print(a[i])
+#     i=i+1
+
+# MOBILE BANKING ATEMPTS LEFT
+ 
+password=1234
+attempts=3
+while attempts>0:
+    user_pass=input("Enter your valid password:")
+    if user_pass==password :
+        print('Welcome')
+        break
+    elif user_pass!=password :
+        attempts=attempts-1
+        print(f"you have {attempts} left")
+    if attempts==0:
+        print("you are blocked")
+        break
