d240d75a3e52612cda8cd7797884d808c7963dcf
ganesh-mahato7 <ganeshmahato1412@gamil.com>
2025-06-06 09:47:30 +0545

    update while

---
+++ while.py
@@ -0,0 +1,33 @@
+# a=[1,2,3,4]
+# i=0
+# while a:
+#     if i==3:
+#      break
+#     print(a[i])
+#     i=i+1
+
+# a=[1,2,3,4]
+# i=0
+# while i<4:
+#     print(a[i])
+#     i=i+1
+
+# a=[1,2,3,4]
+# i=0
+# while i>=0 and i<4:
+#     print(a[i])
+#     i=i+1
+
+# a=[1,2,3,4]
+# i=0
+# while i in range(5):
+#     if i==4:
+#         break
+#     print(a[i])
+#     i=i+1
+
+# a=[1,2,3,4]
+# i=0
+# while i in range(4):
+#     print(a[i])
+#     i=i+1
